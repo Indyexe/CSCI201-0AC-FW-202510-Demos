386d34e3215562ea191c1ee43a50a2de7af3be54
Angela <avenable11@ivytech.edu>
2025-06-09 17:59:33 -0400

    getline with ws

--- 06_09_25/main.cpp
+++ 06_09_25/main.cpp
@@ -6,4 +6,10 @@ int main()
     clockType c;
     c.setTime(5, 41, 22);
     std::cout << c.printTime() << std::endl;
+
+    std::string s;
+    std::cout << "Enter a sentence: ";
+    // std::cin >> s;
+    std::cin >> std::ws;
+    std::getline(std::cin, s);
 }
